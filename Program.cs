7e4101a4dada6a51da35162d19b6b667b458272b
nipa1902 <66264840+nipa1902@users.noreply.github.com>
2020-11-09 16:20:35 +0100

    v 1.0: Initial upload.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace moment1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello world print
+            //Console.WriteLine("Hello, World!");
+
+            // Request user input, save string as variable
+            Console.WriteLine("\nWhich is the greatest of all the cats?");
+            var catname = Console.ReadLine();
+            
+            // Check for correct string answer
+            if (catname == "Boxkat") {
+                Console.WriteLine("You have chosen wisely. Well done!");
+                Console.Write("\nPress any key to exit...");
+
+                //The program wants to close, but we wait for a key input first.
+                Console.ReadKey(true);
+            } 
+            // Wrong answer
+            else {
+                Console.WriteLine("This is incorrect. The greatest of all cats is Boxkat. Goodbye.");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# DT071G mom1 -- Hello, C# and .NET!
+
+### This repo is the first module in the DT071G course, in which I am learning how to use C# and .NET to create web applications. 
